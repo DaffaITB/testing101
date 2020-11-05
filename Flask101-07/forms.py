3068974db207be46dc3b5317220130fc9743072b
DaffaITB <73146551+DaffaITB@users.noreply.github.com>
2020-11-05 23:27:05 +0700

    Add files via upload

---
+++ Flask101-07/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError 
+from Flask101.models import user
+
+class form_registrasi(FlaskForm):
+    username          = StringField("Nama",
+                                     validators = [DataRequired(), Length(min=4, max=30)])
+    email             = StringField("Email",
+                                     validators = [DataRequired(), Email()])
+    password          = PasswordField("Password",
+                                       validators = [DataRequired()])
+    confirm_password  = PasswordField("Konfirmasi password",
+                                       validators = [DataRequired(), EqualTo("password")])
+    submit            = SubmitField("Daftar")
+
+    def validate_username(self, username_baru):
+        kesamaan = user.query.filter_by(username=username_baru.data).first()
+        if kesamaan:
+            raise ValidationError("Username sudah terpakai, silakan coba username lain")
+
+    def validate_email(self, email_baru):
+        kesamaan = user.query.filter_by(email=email_baru.data).first()
+        if kesamaan:
+            raise ValidationError("Email sudah terpakai, silakan coba email lain")
+
+class form_login(FlaskForm):
+    email             = StringField("Email",
+                                     validators = [DataRequired(), Email()])
+    password          = PasswordField("Password",
+                                       validators = [DataRequired()])
+    remember_account  = BooleanField("Ingat saya")
+    submit            = SubmitField("Masuk")
---
+++ Flask101-07/routes.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for, flash, redirect, request
+from Flask101 import app, db, bcrypt, login_manager
+from Flask101.forms import form_registrasi, form_login
+from Flask101.models import user, post
+from flask_login import login_user, logout_user, current_user, login_required
+
+data_post = [
+    {
+        "Judul"     : "Postingan pertama ku",
+        "Nama"      : "Muhammad Daffa Rasyid",
+        "Isi"       : "Saat ini aku sedang mengerjakan tugas PengKom",
+        "Tanggal"   : "28 Oktober 2020"
+    },
+    {
+        "Judul"     : "Postingan kedua ku",
+        "Nama"      : "Ancien Heart",
+        "Isi"       : "*sedang memainkan tablet*",
+        "Tanggal"   : "Tidak diketahui"
+    }
+]
+
+@app.route("/")
+@app.route("/beranda")
+def beranda():
+    return render_template("home1.0.html", title="beranda", P = data_post)
+
+@app.route("/tentang",)
+def tentang():
+    return render_template("about1.0.html", title="tentang")
+
+@app.route("/tentang/pengembang")
+def pengembang():
+    return render_template("aboutcreator1.0.html", title="tentang/pengembang")
+
+@app.route("/daftar", methods = ["GET", "POST"])
+def daftar():
+    if current_user.is_authenticated:
+        return redirect(url_for("beranda"))
+    daftar = form_registrasi()
+    if daftar.validate_on_submit():
+        password_enkripsi = bcrypt.generate_password_hash(daftar.password.data).decode("utf-8")
+        pengguna_baru = user(username=daftar.username.data, email=daftar.email.data, password=password_enkripsi)
+        db.session.add(pengguna_baru)
+        db.session.commit()
+        flash(f"Akun {daftar.username.data} berhasil dibuat! Silakan masuk untuk lanjut.", "success")
+        return redirect(url_for("masuk"))
+    return render_template("register1.0.html", title="daftar", form = daftar)
+
+@app.route("/masuk", methods = ["GET", "POST"])
+def masuk():
+    if current_user.is_authenticated:
+        return redirect(url_for("beranda"))
+    masuk = form_login()
+    if masuk.validate_on_submit():
+        user_tersedia = user.query.filter_by(email=masuk.email.data).first()
+        if user_tersedia and bcrypt.check_password_hash(user_tersedia.password, masuk.password.data):
+            login_user(user_tersedia, remember=masuk.remember_account.data)
+            lanjut = request.args.get("next")
+            return redirect(lanjut) if lanjut else redirect(url_for("beranda"))
+        else:
+            flash("Tidak dapat masuk, silakan cek kembali email dan password Anda!", "danger")
+    return render_template("login1.0.html", title="masuk", form = masuk)
+
+@app.route("/keluar")
+def keluar():
+    logout_user()
+    return redirect(url_for("masuk"))
+
+@app.route("/akun")
+@login_required
+def akun():
+    return render_template("account1.0.html", title="akun")
