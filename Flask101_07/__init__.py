a05a14c7d6522ecea6e2a121384cc7fb3fc7825c
DaffaITB <73146551+DaffaITB@users.noreply.github.com>
2020-11-05 23:51:29 +0700

    Add files via upload

---
+++ Flask101_07/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "thisismysecretkey101"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "masuk"
+login_manager.login_message_category = "info"
+
+from Flask101 import routes
